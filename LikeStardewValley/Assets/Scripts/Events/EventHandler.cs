566048bfb75b1fc071105de33bf2d9fe1a32977d
Damian Kamiński <36849835+Mizuirokoala@users.noreply.github.com>
2022-04-06 15:47:21 +0200

    PlayerAnimationHandler

---
+++ LikeStardewValley/Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,38 @@
+
+public delegate void MovementDelegate(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingToolRight, bool isSwingToolLeft, bool isSwingToolUp, bool isSwingToolDown,
+    bool idleRight, bool idleLEft, bool idleUp, bool idleDown);
+
+
+public static class EventHandler
+{
+    //Movement
+    public static event MovementDelegate MovementEvent;
+
+    //M Event Call
+    public static void MovementCallEvent(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingToolRight, bool isSwingToolLeft, bool isSwingToolUp, bool isSwingToolDown,
+    bool idleRight, bool idleLEft, bool idleUp, bool idleDown)
+    {
+        if (MovementEvent != null)
+            MovementEvent(inputX, inputY, isWalking, isRunning, isIdle, isCarrying,
+            toolEffect,
+            isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+            isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+            isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+            isSwingToolRight, isSwingToolLeft, isSwingToolUp, isSwingToolDown,
+            idleRight, idleLEft, idleUp, idleDown);
+    
+    
+    }
+
+
+}
